3d5e55dfa5300d69e99240b1c955f3fcb68262ea
dev2dynagility <dev2@dynagility.net>
2012-05-29 17:54:22 +0700

    References page :store,model

---
+++ src/sencha/2.0.1/trunk/www/cfa/app/store/References.js
@@ -0,0 +1,20 @@
+Ext.define("cfa.store.References",{
+	extend : "Ext.data.Store",
+	
+	config : {
+		model : "cfa.model.Reference",		
+		proxy : {
+			type: "ajax",
+			url : "/data/References.csv",	
+			reader : {
+				type:"csv"
+			},
+		},
+		sorters: 'title',
+		grouper: {			
+			groupFn: function(record) {
+				return record.get('title')[0];
+			}
+		},
+	}	
+})
