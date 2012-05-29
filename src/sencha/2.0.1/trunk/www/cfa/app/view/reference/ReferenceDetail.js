4ca3a1eb5e352c376aa9719b1e17a72bd996dfe9
dev2dynagility <dev2@dynagility.net>
2012-05-29 17:53:14 +0700

    References page

---
+++ src/sencha/2.0.1/trunk/www/cfa/app/view/reference/ReferenceDetail.js
@@ -0,0 +1,10 @@
+Ext.define('cfa.view.reference.ReferenceDetail',{
+	extend: 'cfa.view.HTMLPanel',
+	alias: 'widget.reference_detail',
+	
+	config:{
+		url: '{url}'	
+	}
+	
+
+})
---
+++ src/sencha/2.0.1/trunk/www/cfa/app/view/reference/ReferencesList.js
@@ -0,0 +1,14 @@
+Ext.define('cfa.view.reference.ReferencesList',{
+	extend: 'Ext.dataview.List',
+	alias: 'widget.references_list',
+	
+	
+		
+	config: {					
+		loadingText: "Loading reference...",
+	    itemTpl: '</pre><div>{title}</div><div><font size="-1">{description}</font></div><pre>',
+		grouped:true,			
+    },
+
+	
+})
