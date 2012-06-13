43d7637a3fbca47f46a415900cd476ff6c91f7da
dev2dynagility <dev2@dynagility.net>
2012-06-13 09:20:40 +0700

    Fixing Contact model for display wrong number issue

--- src/sencha/2.0.1/trunk/www/cfa/app/model/Contact.js
+++ src/sencha/2.0.1/trunk/www/cfa/app/model/Contact.js
@@ -6,8 +6,8 @@ Ext.define('cfa.model.Contact',{
 			{name: 'firstname', type: 'string'},
 			{name: 'lastname', type: 'string'},
 			{name: 'email', type: 'email'},
-			{name: 'office_phone', type: 'int'},
-			{name: 'mobile_phone', type: 'int'},
+			{name: 'office_phone', type: 'string'},
+			{name: 'mobile_phone', type: 'string'},
 			{name: 'departement', type: 'string'}					
 		]	
 	}	
