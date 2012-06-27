87cb1382c1448b88765a5a544bc5fc806e78040c
reedsch <rschrichte@crgt.com>
2012-06-27 10:32:29 -0400

    test

--- src/sencha/2.0.1/trunk/www/cfa/app/view/about/AboutView.js
+++ src/sencha/2.0.1/trunk/www/cfa/app/view/about/AboutView.js
@@ -13,7 +13,6 @@ Ext.define('cfa.view.about.AboutView', {
                 centered: true,
                 itemId: 'versionpanel',
                 html: [
-                    '<p align="center">CFA Mobile</p>',
                 ].join('')
             }
         ]
