9ee43b89064506fc37733075665e002417082a0b
Tin Thai <dev2@dynagility.net>
2012-06-28 16:07:44 +0700

    Addtion report page

---
+++ src/sencha/2.0.1/trunk/www/cfa/app/model/SearchTemplate.js
@@ -0,0 +1,15 @@
+Ext.define('cfa.model.SearchTemplate',{
+	extend: 'Ext.data.Model',
+	
+	config:{
+		indentifier: 'uuid',
+		fields: [
+			{name: 'text', type: 'string'},
+			{name: 'description', type: 'string'},
+			{name: 'queryType', type: 'string'},
+			{name: 'queryParam', type: 'string'}
+		]
+		
+	}
+	
+})
---
+++ src/sencha/2.0.1/trunk/www/cfa/app/view/search/SearchView.js
@@ -0,0 +1,33 @@
+var store = Ext.getStore("Contacts");
+Ext.define('cfa.view.search.SearchView', {
+    extend: 'Ext.navigation.View',
+    xtype: 'searchview',
+
+	config:{
+		title: 'Search',
+		navigationBar:{
+			items: [
+				{
+					xtype: 'searchfield',
+					id: 'searchinputfield',
+					align: 'right'
+				}
+			]		
+		},
+		
+		items: [
+			{
+				xtype: 'list',
+				title: 'Search Templates',
+				id: 'searchtemplatelist',
+				itemTpl: '</pre><div>{text}</div><div><font size="-1">{description}</font></div><pre>',
+				store: 'SearchTemplates',
+				
+			}
+		]
+	},
+	
+});
+
+
+
